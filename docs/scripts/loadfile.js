61029a1e638dc8cab667f29772fdfef5f0b5b1d0
捩麻 <32641080+NejimakiBoy@users.noreply.github.com>
2022-01-05 15:20:38 +0900

    update

--- docs/scripts/loadfile.js
+++ docs/scripts/loadfile.js
@@ -1,30 +1,52 @@
 async function main() {
 
-  const file = await fetch(`../medias/teapotWithoutF.obj`);
-  console.log(file.result);
-  const content = await file.text();
-  console.log(content);
+  const file = await fetch(`../medias/teapot.obj`);
+  //console.log(file.result);
+  const text = await file.text();
+  //console.log(text);
 
-  paeseText(content);
+  var v = [];
+  var f = []
+
+  paeseText(text, v, f);
+
+  var sortedV = []
+  sortV(sortedV, v, f);
+  console.log("sortedV: " + sortedV)
 }
 
-function paeseText(text) {
+function paeseText(text, v, f) {
   const lines = text.split(`\n`);
-  console.log(lines);
-
-  const vertices = [];
+  //console.log(lines);
 
   for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
     const line = lines[lineNo].trim();
-    console.log(line);
-    const parts = line.split(/\s+/).slice(1);
-    console.log(parts);
-    for (let i = 0; i < parts.length; ++i) {
-      vertices.push(parts[i]);
-    }
-    console.log(vertices);
+    //console.log("line " + lineNo + ": " + line);
 
+    if ("v" == line.charAt(0)) {
+      const parts = line.split(/\s+/).slice(1);
+      v.push(parts);
+    }
+
+    else if ("f" == line.charAt(0)) {
+      const parts = line.split(/\s+/).slice(1);
+      for (let partsNo = 0; partsNo < parts.length; ++partsNo) {
+        f.push(parts[partsNo]);
+      }
+    }
   }
 }
+
+function sortV(sortedV, v, f) {
+  for (let fNo = 0; fNo < f.length; ++fNo) {
+    var vNo = Number(f[fNo]);
+    if (v[vNo] != void 0) {
+      for (var i = 0; i < v[vNo].length; ++i) {
+        sortedV.push(v[vNo][i]);
+      }
+    }
+  }
+}
+
 
 main();
