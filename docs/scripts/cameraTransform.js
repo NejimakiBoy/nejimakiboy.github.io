7d54fee93b86b1b340504d22be1199d219fbbe01
捩麻 <32641080+NejimakiBoy@users.noreply.github.com>
2022-01-09 17:11:14 +0900

    update

--- docs/scripts/cameraTransform.js
+++ docs/scripts/cameraTransform.js
@@ -100,9 +100,14 @@ async function main() {
   }
 
   // トランスフォームを設定
-  var translation = [0, -0.3, -2];
+  var translation = [0, -1, 0];
   var rotation = [degToRad(0), degToRad(0), degToRad(0)];
-  var scale = [0.2, 0.2, 0.2];
+  var scale = [1, 1, 1];
+
+  var cameraAngleRadians = degToRad(0);
+  var radius = 3;
+  var cameraRadius = 10;
+
   var fieldOfViewRadians = degToRad(60);
   var change = 1;
 
@@ -121,17 +126,17 @@ async function main() {
     // 現在時間は次のフレームで経過時間として使う
     then = now;
 
-    // 角度を更新する
-    rotation[1] += rotationSpeed * deltaTime;
+    // カメラ角度を更新する
+    cameraAngleRadians += rotationSpeed * deltaTime;
 
     // 視野角を更新する
-    fieldOfViewRadians += 0.005 * change;
+    cameraRadius += 0.05 * change;
 
     // 視野角が一定の範囲より大きかったり小さくならないようにする
-    if (fieldOfViewRadians >= 2) {
+    if (cameraRadius >= 30) {
       change = -1;
     }
-    else if (fieldOfViewRadians <= 0) {
+    else if (cameraRadius <= 3) {
       change = 1;
     }
 
@@ -165,7 +170,7 @@ async function main() {
 
     // バッファーからのデータのとり方を設定する
     var size = 3; // 呼び出すごとに3つづつ取る（x, y, z）
-    var type = gl.FLOAT; // データのデータ型
+    var type = gl.FLOAT; // データ型
     var normalize = false; // 正規化の有無
     var stride = 0; // シェーダーを呼び出すごとに進む距離らしい 0 = size * sizeof(type) よくわからない
     var offset = 0; // バッファーのどこから読み始めるか
@@ -187,28 +192,50 @@ async function main() {
     var zNear = 1; // zNearClipの距離
     var zFar = 200; // zFarClipの距離
 
-    // 各行列の計算と掛け合わせ
-    // 掛け合わせの順番を変えると結果が変わってしまうので気をつける
-    // 特に意図がなければ以下の順番でやるべき
-    var matrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
-    matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
-    matrix = m4.xRotate(matrix, rotation[0]);
-    matrix = m4.yRotate(matrix, rotation[1]);
-    matrix = m4.zRotate(matrix, rotation[2]);
-    matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+    // プロジェクション行列
+    var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
 
-    // 移動回転の基準点を移動
-    var movePivotMatrix = m4.translation(0, 0, 0, 0);
-    matrix = m4.multiply(matrix, movePivotMatrix);
+    // カメラのトランスフォーム行列
+    var cameraMatrix = m4.yRotation(cameraAngleRadians);
+    cameraMatrix = m4.translate(cameraMatrix, 0, 0, cameraRadius);
 
-    // 行列を渡す
-    gl.uniformMatrix4fv(matrixUniformLocation, false, matrix);
 
-    // 描画
-    var primitiveType = gl.TRIANGLES; // プリミティブを選ぶ
-    var offset = 0; // 頂点の配列のどこから読み出すか 最初に読み出される配列要素の添字
-    var count = vertexArray.length / 3; // 描画するプリミティブの数を指定する 今回は三角形なので 頂点数/3 になる
-    gl.drawArrays(primitiveType, offset, count);
+    // ビュー行列を作る
+    var viewMatrix = m4.inverse(cameraMatrix);
+
+    // ビュープロジェクション行列を計算
+    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+
+    // ティーポットの数
+    var teapots = 3
+
+    for (let i = 0; i < teapots; ++i) {
+
+      // 円形に配置
+      var angle = i * Math.PI * 2 / teapots;
+      var x = Math.cos(angle) * radius;
+      var y = Math.sin(angle) * radius;
+
+      // トランスフォームの行列を計算
+      var matrix = m4.translate(viewProjectionMatrix, x, translation[1], y);
+      matrix = m4.xRotate(matrix, rotation[0]);
+      matrix = m4.yRotate(matrix, rotation[1]);
+      matrix = m4.zRotate(matrix, rotation[2]);
+      matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+
+      // 移動回転の基準点を移動
+      var movePivotMatrix = m4.translation(0, 0, 0, 0);
+      matrix = m4.multiply(matrix, movePivotMatrix);
+
+      // 行列を渡す
+      gl.uniformMatrix4fv(matrixUniformLocation, false, matrix);
+
+      // 描画
+      var primitiveType = gl.TRIANGLES; // プリミティブを選ぶ
+      var offset = 0; // 頂点の配列のどこから読み出すか 最初に読み出される配列要素の添字
+      var count = vertexArray.length / 3; // 描画するプリミティブの数を指定する 今回は三角形なので 頂点数/3 になる
+      gl.drawArrays(primitiveType, offset, count);
+    }
 
     requestAnimationFrame(drawScene);
   }
@@ -293,16 +320,16 @@ async function parseObj(text, vertexArray) {
 
 function resize(canvas) {
   // ブラウザがcanvasを表示しているサイズを調べる。
-  var displayWidth  = canvas.clientWidth;
+  var displayWidth = canvas.clientWidth;
   var displayHeight = canvas.clientHeight;
- 
+
   // canvasの「描画バッファーのサイズ」と「表示サイズ」が異なるかどうか確認する。
-  if (canvas.width  != displayWidth ||
-      canvas.height != displayHeight) {
- 
+  if (canvas.width != displayWidth ||
+    canvas.height != displayHeight) {
+
     // サイズが違っていたら、描画バッファーのサイズを
     // 表示サイズと同じサイズに合わせる。
-    canvas.width  = displayWidth;
+    canvas.width = displayWidth;
     canvas.height = displayHeight;
   }
 }
@@ -446,6 +473,102 @@ var m4 = {
 
   scale: function (m, sx, sy, sz) {
     return m4.multiply(m, m4.scaling(sx, sy, sz));
+  },
+
+  inverse: function (m) {
+    var m00 = m[0 * 4 + 0];
+    var m01 = m[0 * 4 + 1];
+    var m02 = m[0 * 4 + 2];
+    var m03 = m[0 * 4 + 3];
+    var m10 = m[1 * 4 + 0];
+    var m11 = m[1 * 4 + 1];
+    var m12 = m[1 * 4 + 2];
+    var m13 = m[1 * 4 + 3];
+    var m20 = m[2 * 4 + 0];
+    var m21 = m[2 * 4 + 1];
+    var m22 = m[2 * 4 + 2];
+    var m23 = m[2 * 4 + 3];
+    var m30 = m[3 * 4 + 0];
+    var m31 = m[3 * 4 + 1];
+    var m32 = m[3 * 4 + 2];
+    var m33 = m[3 * 4 + 3];
+    var tmp_0 = m22 * m33;
+    var tmp_1 = m32 * m23;
+    var tmp_2 = m12 * m33;
+    var tmp_3 = m32 * m13;
+    var tmp_4 = m12 * m23;
+    var tmp_5 = m22 * m13;
+    var tmp_6 = m02 * m33;
+    var tmp_7 = m32 * m03;
+    var tmp_8 = m02 * m23;
+    var tmp_9 = m22 * m03;
+    var tmp_10 = m02 * m13;
+    var tmp_11 = m12 * m03;
+    var tmp_12 = m20 * m31;
+    var tmp_13 = m30 * m21;
+    var tmp_14 = m10 * m31;
+    var tmp_15 = m30 * m11;
+    var tmp_16 = m10 * m21;
+    var tmp_17 = m20 * m11;
+    var tmp_18 = m00 * m31;
+    var tmp_19 = m30 * m01;
+    var tmp_20 = m00 * m21;
+    var tmp_21 = m20 * m01;
+    var tmp_22 = m00 * m11;
+    var tmp_23 = m10 * m01;
+
+    var t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) -
+      (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+    var t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) -
+      (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+    var t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) -
+      (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+    var t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) -
+      (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+
+    var d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+
+    return [
+      d * t0,
+      d * t1,
+      d * t2,
+      d * t3,
+      d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) -
+        (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
+      d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) -
+        (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
+      d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) -
+        (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
+      d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) -
+        (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
+      d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) -
+        (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
+      d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) -
+        (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
+      d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) -
+        (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
+      d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) -
+        (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
+      d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) -
+        (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
+      d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) -
+        (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
+      d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) -
+        (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
+      d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) -
+        (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02))
+    ];
+  },
+
+  vectorMultiply: function (v, m) {
+    var dst = [];
+    for (var i = 0; i < 4; ++i) {
+      dst[i] = 0.0;
+      for (var j = 0; j < 4; ++j) {
+        dst[i] += v[j] * m[j * 4 + i];
+      }
+    }
+    return dst;
   },
 
 };
